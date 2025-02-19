e0286e41f4f7c23c0dd14259b46d2ec3ddb93bbe
arya. <115932787+arvedd@users.noreply.github.com>
2025-02-19 17:21:39 +0700

    Add Ping Pong game mechanics and scoring system - 2025.2.19
    - Implemented player movement script
    - Added scoring system
    - Adjusted game mechanics

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,68 +1,69 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-
-    void Start() {
-        
-        rb2d = GetComponent<Rigidbody2D>();
-        Invoke("GoBall", 2);
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); //mengambil rigidbody component dari sebuah bole
+        Invoke("GoBall", 2); //memanggil function GoBall dlm 2 detik
     }
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2); //akan random nilai diantara 0-1
+        if (rand < 1)
+        { 
+            rb2d.AddForce(new Vector2(20, -15)); //add force memberikan tenaga
+						//liat doc add force disini https://docs.unity3d.com/ScriptReference/Rigidbody.AddForce.html
 
-    void GoBall() {
-
-        float rand = Random.Range(0, 2);
-
-        if(rand < 1) {
-            
-            rb2d.AddForce(new Vector2(20, -15));
-        
-        }else {
-
+        }
+        else
+        {
             rb2d.AddForce(new Vector2(-20, -15));
-
         }
     }
 
-    void ResetBall() {
-
-        rb2d.linearVelocity = Vector2.zero;
+    void ResetBall() //ini kita buat nilai transform jadi 0
+    {
+        rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
     }
 
-    void RestartGame() {
-
+    void RestartGame()
+    {
+        //Debug.Log("Restart!");
         ResetBall();
         Invoke("GoBall", 1);
-
     }
 
-    [SerializeField] private int wallCollision;
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] private int wallCollisionCount;
+    private void OnCollisionEnter2D(Collision2D coll)
     {
-        if(collision.gameObject.name == "King") {
+        if (coll.gameObject.name == "King") //jika terkena player
+        {
+            // Debug.Log("King Punch!");
+            rb2d.AddForce(new Vector2(20,-15));
+            wallCollisionCount = 0;
 
-            rb2d.AddForce(new Vector2(20, -15));
-            wallCollision = 0;
-
-        }else if(collision.gameObject.name == "Pig") {
-
-            rb2d.AddForce(new Vector2(-20, -15));
-            wallCollision = 0;
-
-        }else {
-
-            wallCollision = wallCollision + 1;
-            Debug.Log("Wall Collision = " + wallCollision);
+        }
+        else if (coll.gameObject.name == "Enemy") //jika terkena enemy
+        {
+            // Debug.Log("Pig Punch!");
+            rb2d.AddForce(new Vector2(-20,-15));
+            wallCollisionCount = 0;
+        }
+        else //jika terkena wall
+        {
+            wallCollisionCount = wallCollisionCount + 1;
+            Debug.Log("Wall Collision! = " + wallCollisionCount);
             
-            if(wallCollision > 6) {
+            if (wallCollisionCount > 6) {
 
                 GoBall();
-            }
+            } 
         }
-
     }
+
 }
